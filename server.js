815a87fbdaf253d9614cd8e6736f0688a06ea2e7
Miguel Fraga <mfraga@conectium.com>
2020-01-30 12:49:59 -0400

    Se agregaron los modulos  path, serve-static y mysql al paquete de dependencias

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 require("dotenv").config();
-import compression from "compression";
-import express from 'express';
+const express = require("express");
 const app = express();
 const port = process.env.PORT;
+var compression = require('compression');
+var path = require("path");
+var bodyParser = require('body-parser');
 
 app.use(compression());
+app.use("/static", express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Hello World!'));
 

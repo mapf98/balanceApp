c2e35ba8a4c47c56cfd96b67e29f84b9ac167e00
Miguel Fraga <mfraga@conectium.com>
2020-01-30 14:04:24 -0400

    Servidor con peticiones get verificadas con conexion a BD

--- server.js
+++ server.js
@@ -5,12 +5,79 @@ const port = process.env.PORT;
 var compression = require('compression');
 var path = require("path");
 var bodyParser = require('body-parser');
+var errorhandler = require("errorhandler");
+var isProduction = process.env.NODE_ENV === "production";
+
+//Conexion BD
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "MIGuel33&&",
+  database: "dbexpress",
+  insecureAuth: true
+});
+
+connection.connect();
+//
+
+app.get("/prueba", function (req, res) {
+  let personas = [];
+  connection.query("SELECT * FROM PERSONA AS personas", function(
+    error,
+    results,
+    fields
+  ) {
+    if (error) {
+      res.send(error);
+    } else {
+      personas = results;
+      res.json({ data: personas });
+    }
+  });
+});
 
 app.use(compression());
 app.use("/static", express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello World!'));
+if (!isProduction) {
+  app.use(errorhandler());
+}
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (!isProduction) {
+  app.use(function(err, req, res, next) {
+    console.log(err.stack);
+
+    res.status(err.status || 500);
+
+    res.json({'errors': {
+      message: err.message,
+      error: err
+    }});
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({'errors': {
+    message: err.message,
+    error: {}
+  }});
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));

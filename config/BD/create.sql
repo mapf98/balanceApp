51e5bb4c9a4f4bfa036fef19d9527d6dc592daa4
Miguel Fraga <mfraga@conectium.com>
2020-03-18 19:18:34 -0400

    Modificaciones en ER, se incluyeron las entidades de Lugar, Feedback, Categoria, Estatus, Historial_Moneda y Perfil

--- config/BD/create.sql
+++ config/BD/create.sql
@@ -1,5 +1,13 @@
 /*SCRIPTS DE CREACIÓN*/
 
+/*AGREGAR EN BACKEND*/
+CREATE TABLE ESTATUS (
+	status_id INTEGER NOT NULL AUTO_INCREMENT,
+    status_name VARCHAR(50) NOT NULL,
+	PRIMARY KEY (status_id)
+);
+
+/*ARREGLAR EN BACKEND*/
 CREATE TABLE USUARIO (
 	user_id INTEGER NOT NULL AUTO_INCREMENT,
     user_first_name VARCHAR(20) NOT NULL,
@@ -8,44 +16,109 @@ CREATE TABLE USUARIO (
     user_alias VARCHAR(50) NOT NULL UNIQUE,
     user_birthdate DATE NOT NULL,
     user_password VARCHAR(12) NOT NULL,
+    user_create_date DATE NOT NULL,
     PRIMARY KEY (user_id)
 );
 
-CREATE TABLE BANCO (
-	bank_id INTEGER NOT NULL AUTO_INCREMENT,
-    bank_name VARCHAR(50) NOT NULL,
-	PRIMARY KEY (bank_id)
-);
-
+/*ARREGLAR EN BACKEND*/
 CREATE TABLE MONEDA (
 	currency_id INTEGER NOT NULL AUTO_INCREMENT,
     currency_name VARCHAR(20) NOT NULL,
     currency_symbol VARCHAR(5) NOT NULL UNIQUE,
     currency_iso_code VARCHAR(5) NOT NULL,
+    currency_amount_one_dollar_equivalent DOUBLE NOT NULL,
 	PRIMARY KEY (currency_id)
 );
 
+/*AGREGAR EN BACKEND*/
+CREATE TABLE HISTORIAL_MONEDA (
+	currency_history_id INTEGER NOT NULL AUTO_INCREMENT,
+    currency_history_amount_one_dollar_equivalent DOUBLE NOT NULL,
+    currency_history_date DATE NOT NULL,
+    fk_currency_id INTEGER NOT NULL,
+	PRIMARY KEY (currency_history_id),
+    FOREIGN KEY FK_HISTORY_CURRENCY_CURRENCY_ID (fk_currency_id) REFERENCES MONEDA (currency_id)
+);
+
+/*AGREGAR EN BACKEND*/
+CREATE TABLE FEEDBACK (
+	feedback_id INTEGER NOT NULL AUTO_INCREMENT,
+    feedback_description VARCHAR(100),
+    feedback_rate DOUBLE NOT NULL,
+    feedback_date DATE NOT NULL,
+	fk_user_id INTEGER NOT NULL,
+    PRIMARY KEY (feedback_id),
+    FOREIGN KEY FK_FEEDBACK_USER_ID (fk_user_id) REFERENCES USUARIO (user_id)
+);
+
+/*AGREGAR EN BACKEND*/
+CREATE TABLE LUGAR (
+	place_id INTEGER NOT NULL AUTO_INCREMENT,
+    place_name VARCHAR(50) NOT NULL,
+    place_type VARCHAR(3) NOT NULL,
+    fk_place_id INTEGER,
+	PRIMARY KEY (place_id),
+    FOREIGN KEY FK_PLACE_PLACE_ID (fk_place_id) REFERENCES LUGAR (place_id),
+    CONSTRAINT CHECK_PLACE_TYPE CHECK (place_type in ('PAI','EST','MUN'))
+);
+
+/*ARREGLAR EN BACKEND*/
+CREATE TABLE BANCO (
+	bank_id INTEGER NOT NULL AUTO_INCREMENT,
+    bank_name VARCHAR(50) NOT NULL,
+    fk_place_id INTEGER NOT NULL,
+	PRIMARY KEY (bank_id),
+    FOREIGN KEY FK_BANK_PLACE_ID (fk_place_id) REFERENCES LUGAR (place_id)
+);
+
+/*AGREGAR EN BACKEND*/
+CREATE TABLE PERFIL (
+	profile_id INTEGER NOT NULL AUTO_INCREMENT,
+    profile_name VARCHAR(25) NOT NULL,
+    profile_type VARCHAR(3) NOT NULL,
+    profile_create_date DATE NOT NULL,
+    fk_user_id INTEGER NOT NULL,
+	PRIMARY KEY (profile_id),
+    FOREIGN KEY FK_PROFILE_USER_ID (fk_user_id) REFERENCES USUARIO (user_id),
+    CONSTRAINT CHECK_PROFILE_TYPE CHECK (profile_type in ('PER','COM','OTR'))
+);
+
+/*AGREGAR EN BACKEND*/
+CREATE TABLE CATEGORIA (
+	category_id INTEGER NOT NULL AUTO_INCREMENT,
+    category_name VARCHAR(50) NOT NULL,
+	PRIMARY KEY (category_id)
+);
+
+/*ARREGLAR EN BACKEND*/
 CREATE TABLE CUENTA (
 	account_id INTEGER NOT NULL AUTO_INCREMENT,
     account_type VARCHAR(3) NOT NULL,
     account_reference VARCHAR(30) NOT NULL,
-    fk_user_id INTEGER NOT NULL,
+    account_create_date DATE NOT NULL,
+    fk_profile_id INTEGER NOT NULL,
     fk_bank_id INTEGER,
+    fk_status_id INTEGER NOT NULL,
     fk_currency_id INTEGER NOT NULL,
 	PRIMARY KEY (account_id),
-    FOREIGN KEY FK_ACCOUNT_USER_ID (fk_user_id) REFERENCES USUARIO (user_id),
+    FOREIGN KEY FK_ACCOUNT_PROFILE_ID (fk_profile_id) REFERENCES PERFIL (profile_id),
     FOREIGN KEY FK_ACCOUNT_BANK_ID (fk_bank_id) REFERENCES BANCO (bank_id),
     FOREIGN KEY FK_ACCOUNT_CURRENCY_ID (fk_currency_id) REFERENCES MONEDA (currency_id),
+    FOREIGN KEY FK_ACCOUNT_STATUS_ID (fk_status_id) REFERENCES ESTATUS (status_id),
     CONSTRAINT CHECK_ACCOUNT_TYPE CHECK (account_type in ('DIG','EFE','CRY'))
 );
 
+/*ARREGLAR EN BACKEND*/
 CREATE TABLE TRANSACCION (
 	transaction_id INTEGER NOT NULL AUTO_INCREMENT,
     transaction_account_total DOUBLE NOT NULL,
     transaction_amount DOUBLE NOT NULL,
     transaction_concept VARCHAR(150) NOT NULL,
     transaction_date DATE NOT NULL,
+    transaction_create_date DATE NOT NULL,
     fk_account_id INTEGER NOT NULL,
+    fk_category_id INTEGER NOT NULL,
 	PRIMARY KEY (transaction_id),
-    FOREIGN KEY FK_TRANSACTION_ACCOUNT_ID (fk_account_id) REFERENCES CUENTA (account_id)
+    FOREIGN KEY FK_TRANSACTION_ACCOUNT_ID (fk_account_id) REFERENCES CUENTA (account_id),
+    FOREIGN KEY FK_TRANSACTION_CATEGORY_ID (fk_category_id) REFERENCES CATEGORIA (category_id)
 );
--- config/BD/insert.sql
+++ config/BD/insert.sql
@@ -1,41 +1,89 @@
 /*SCRIPTS DE INSERCIÓN*/
 
-/*BANCO*/
-INSERT INTO BANCO (bank_name) VALUES ('Bank of America');
-INSERT INTO BANCO (bank_name) VALUES ('BBVA');
-INSERT INTO BANCO (bank_name) VALUES ('Mercantil');
-INSERT INTO BANCO (bank_name) VALUES ('Banesco');
-INSERT INTO BANCO (bank_name) VALUES ('BFC');
-SELECT * FROM BANCO;
-
-/*MONEDA*/
-INSERT INTO MONEDA (currency_name, currency_symbol, currency_iso_code) VALUES ('Bolivares', 'Bs', 'VES');
-INSERT INTO MONEDA (currency_name, currency_symbol, currency_iso_code) VALUES ('Dolares', '$', 'USD');
-INSERT INTO MONEDA (currency_name, currency_symbol, currency_iso_code) VALUES ('Bolivares', '€', 'EUR');
-INSERT INTO MONEDA (currency_name, currency_symbol, currency_iso_code) VALUES ('Yenes', '¥', 'JPY');
-INSERT INTO MONEDA (currency_name, currency_symbol, currency_iso_code) VALUES ('Libra Esterlina', '£', 'GBP');
-SELECT * FROM MONEDA;
+/*CATEGORIA*/
+INSERT INTO CATEGORIA (category_name) VALUES ('Alimentos');
+INSERT INTO CATEGORIA (category_name) VALUES ('Estudios');
+INSERT INTO CATEGORIA (category_name) VALUES ('Vestimenta');
+INSERT INTO CATEGORIA (category_name) VALUES ('Hogar');
+INSERT INTO CATEGORIA (category_name) VALUES ('Vehículos');
+SELECT * FROM CATEGORIA;
 
 /*USUARIO*/
-INSERT INTO USUARIO (user_first_name, user_last_name, user_email, user_alias, user_birthdate, user_password) VALUES ('Miguel' , 'Fraga', 'mpfraga98@gmail.com', 'mpfraga98', '1998-04-03', 'admin1');
-INSERT INTO USUARIO (user_first_name, user_last_name, user_email, user_alias, user_birthdate, user_password) VALUES ('Alba', 'Silvestre', 'alba.sofia.n.n@gmail.com', 'albasofia', '1999-01-31', 'admin2');
-INSERT INTO USUARIO (user_first_name, user_last_name, user_email, user_alias, user_birthdate, user_password) VALUES ('Josefa', 'Fraga', 'jfraga2023@gmail.com', 'jfraga2023', '1963-12-20', 'admin3');
-INSERT INTO USUARIO (user_first_name, user_last_name, user_email, user_alias, user_birthdate, user_password) VALUES ('Jesús', 'Chang', 'zchangvx@hotmail.com', 'zchang', '1997-10-25', 'admin4');
-INSERT INTO USUARIO (user_first_name, user_last_name, user_email, user_alias, user_birthdate, user_password) VALUES ('Diego', 'Di Salvatore', 'disalvatorediego@gmail.com', 'disalvatore', '1998-06-02', 'admin5');
+INSERT INTO USUARIO (user_first_name, user_last_name, user_email, user_alias, user_birthdate, user_password, user_create_date) VALUES ('Miguel' , 'Fraga', 'mpfraga98@gmail.com', 'mpfraga98', '1998-04-03', 'admin1', '2020-04-03');
+INSERT INTO USUARIO (user_first_name, user_last_name, user_email, user_alias, user_birthdate, user_password, user_create_date) VALUES ('Alba', 'Silvestre', 'alba.sofia.n.n@gmail.com', 'albasofia', '1999-01-31', 'admin2', '2020-04-12');
+INSERT INTO USUARIO (user_first_name, user_last_name, user_email, user_alias, user_birthdate, user_password, user_create_date) VALUES ('Josefa', 'Fraga', 'jfraga2023@gmail.com', 'jfraga2023', '1963-12-20', 'admin3', '2020-04-13');
+INSERT INTO USUARIO (user_first_name, user_last_name, user_email, user_alias, user_birthdate, user_password, user_create_date) VALUES ('Jesús', 'Chang', 'zchangvx@hotmail.com', 'zchang', '1997-10-25', 'admin4', '2020-04-30');
+INSERT INTO USUARIO (user_first_name, user_last_name, user_email, user_alias, user_birthdate, user_password, user_create_date) VALUES ('Diego', 'Di Salvatore', 'disalvatorediego@gmail.com', 'disalvatore', '1998-06-02', 'admin5', '2020-04-02');
 SELECT * FROM USUARIO;
 
+/*FEEDBACK*/
+INSERT INTO FEEDBACK (feedback_description, feedback_rate, feedback_date, fk_user_id) VALUES ('Muy bueno, excelente!', '5', '2020-10-20', 1);
+INSERT INTO FEEDBACK (feedback_description, feedback_rate, feedback_date, fk_user_id) VALUES ('Agregar mas categorias', '5', '2020-12-22', 2);
+INSERT INTO FEEDBACK (feedback_description, feedback_rate, feedback_date, fk_user_id) VALUES ('Color muy claro', '5', '2020-07-12', 3);
+INSERT INTO FEEDBACK (feedback_description, feedback_rate, feedback_date, fk_user_id) VALUES ('Falta configuracion', '5', '2020-04-22', 1);
+INSERT INTO FEEDBACK (feedback_description, feedback_rate, feedback_date, fk_user_id) VALUES ('Muy lento', '5', '2020-06-04', 5);
+SELECT * FROM FEEDBACK;
+
+/*LUGAR*/
+INSERT INTO LUGAR (place_name, place_type, fk_place_id) VALUES ('Venezuela', 'PAI', NULL);
+INSERT INTO LUGAR (place_name, place_type, fk_place_id) VALUES ('Alemania', 'PAI', NULL);
+INSERT INTO LUGAR (place_name, place_type, fk_place_id) VALUES ('España', 'PAI', NULL);
+INSERT INTO LUGAR (place_name, place_type, fk_place_id) VALUES ('Puerto Rico', 'PAI', NULL);
+INSERT INTO LUGAR (place_name, place_type, fk_place_id) VALUES ('Luxemburgo', 'PAI', NULL);
+SELECT * FROM LUGAR;
+
+/*BANCO*/
+INSERT INTO BANCO (bank_name, fk_place_id) VALUES ('Bank of America', 3);
+INSERT INTO BANCO (bank_name, fk_place_id) VALUES ('BBVA', 1);
+INSERT INTO BANCO (bank_name, fk_place_id) VALUES ('Mercantil', 1);
+INSERT INTO BANCO (bank_name, fk_place_id) VALUES ('Banesco', 1);
+INSERT INTO BANCO (bank_name, fk_place_id) VALUES ('BFC', 5);
+SELECT * FROM BANCO;
+
+/*ESTATUS*/
+INSERT INTO ESTATUS (status_name) VALUES ('OPEN');
+INSERT INTO ESTATUS (status_name) VALUES ('CLOSE');
+INSERT INTO ESTATUS (status_name) VALUES ('DISCONTINUED');
+INSERT INTO ESTATUS (status_name) VALUES ('REMOVE');
+INSERT INTO ESTATUS (status_name) VALUES ('ON HOLD');
+SELECT * FROM ESTATUS;
+
+/*MONEDA*/
+INSERT INTO MONEDA (currency_name, currency_symbol, currency_iso_code, currency_amount_one_dollar_equivalent) VALUES ('Bolivares', 'Bs', 'VES', 73000);
+INSERT INTO MONEDA (currency_name, currency_symbol, currency_iso_code, currency_amount_one_dollar_equivalent) VALUES ('Dolares', '$', 'USD', 1);
+INSERT INTO MONEDA (currency_name, currency_symbol, currency_iso_code, currency_amount_one_dollar_equivalent) VALUES ('Bolivares', '€', 'EUR', 0.9);
+INSERT INTO MONEDA (currency_name, currency_symbol, currency_iso_code, currency_amount_one_dollar_equivalent) VALUES ('Yenes', '¥', 'JPY', 1.8);
+INSERT INTO MONEDA (currency_name, currency_symbol, currency_iso_code, currency_amount_one_dollar_equivalent) VALUES ('Libra Esterlina', '£', 'GBP', 1.2);
+SELECT * FROM MONEDA;
+
+/*HISTORIAL_MONEDA*/
+INSERT INTO HISTORIAL_MONEDA (currency_history_amount_one_dollar_equivalent, currency_history_date, fk_currency_id) VALUES ('74000', '2020-03-18', 1);
+INSERT INTO HISTORIAL_MONEDA (currency_history_amount_one_dollar_equivalent, currency_history_date, fk_currency_id) VALUES ('75000', '2020-03-18', 1);
+INSERT INTO HISTORIAL_MONEDA (currency_history_amount_one_dollar_equivalent, currency_history_date, fk_currency_id) VALUES ('78000', '2020-03-18', 1);
+INSERT INTO HISTORIAL_MONEDA (currency_history_amount_one_dollar_equivalent, currency_history_date, fk_currency_id) VALUES ('84000', '2020-03-18', 1);
+INSERT INTO HISTORIAL_MONEDA (currency_history_amount_one_dollar_equivalent, currency_history_date, fk_currency_id) VALUES ('101000', '2020-03-18', 1);
+SELECT * FROM HISTORIAL_MONEDA;
+
+/*PERFIL*/
+INSERT INTO PERFIL (profile_name, profile_type, profile_create_date, fk_user_id) VALUES ('Abuelo', 'PER', '2020-03-10', '1');
+INSERT INTO PERFIL (profile_name, profile_type, profile_create_date, fk_user_id) VALUES ('Proindal', 'COM', '2020-03-10', '1');
+INSERT INTO PERFIL (profile_name, profile_type, profile_create_date, fk_user_id) VALUES ('HH', 'COM', '2020-03-10', '1');
+INSERT INTO PERFIL (profile_name, profile_type, profile_create_date, fk_user_id) VALUES ('Mama', 'PER', '2020-03-10', '1');
+INSERT INTO PERFIL (profile_name, profile_type, profile_create_date, fk_user_id) VALUES ('Ramonete', 'OTR', '2020-03-10', '1');
+SELECT * FROM PERFIL;
+
 /*CUENTA*/
-INSERT INTO CUENTA (account_type, account_reference, fk_user_id, fk_bank_id, fk_currency_id) VALUES ('DIG', '234DVC', 1, 1, 2);
-INSERT INTO CUENTA (account_type, account_reference, fk_user_id, fk_bank_id, fk_currency_id) VALUES ('EFE', 'Chirulinos', 1, NULL, 1);
-INSERT INTO CUENTA (account_type, account_reference, fk_user_id, fk_bank_id, fk_currency_id) VALUES ('EFE', 'Proindal', 2, NULL, 1);
-INSERT INTO CUENTA (account_type, account_reference, fk_user_id, fk_bank_id, fk_currency_id) VALUES ('DIG', 'AAFS33', 2, 1, 2);
-INSERT INTO CUENTA (account_type, account_reference, fk_user_id, fk_bank_id, fk_currency_id) VALUES ('DIG', '0092SD', 4, 2, 3);
+INSERT INTO CUENTA (account_type, account_reference, account_create_date, fk_profile_id, fk_bank_id, fk_currency_id, fk_status_id) VALUES ('DIG', '234DVC', '2020-04-11', 1, 1, 2, 1);
+INSERT INTO CUENTA (account_type, account_reference, account_create_date, fk_profile_id, fk_bank_id, fk_currency_id, fk_status_id) VALUES ('EFE', 'Chirulinos', '2020-04-11', 1, NULL, 1, 2);
+INSERT INTO CUENTA (account_type, account_reference, account_create_date, fk_profile_id, fk_bank_id, fk_currency_id, fk_status_id) VALUES ('EFE', 'Proindal', '2020-04-11', 2, NULL, 1, 3);
+INSERT INTO CUENTA (account_type, account_reference, account_create_date, fk_profile_id, fk_bank_id, fk_currency_id, fk_status_id) VALUES ('DIG', 'AAFS33', '2020-04-11', 2, 1, 2, 1);
+INSERT INTO CUENTA (account_type, account_reference, account_create_date, fk_profile_id, fk_bank_id, fk_currency_id, fk_status_id) VALUES ('DIG', '0092SD', '2020-04-11', 4, 2, 3, 3);
 SELECT * FROM CUENTA;
 
 /*TRANSACCION*/
-INSERT INTO TRANSACCION (transaction_account_total, transaction_amount, transaction_concept, transaction_date, fk_account_id) VALUES (20.32, -10, 'Primer pago de clases', '2020-01-02', 1);
-INSERT INTO TRANSACCION (transaction_account_total, transaction_amount, transaction_concept, transaction_date, fk_account_id) VALUES (100.9, 120, 'Carne', '2020-04-01', 4);
-INSERT INTO TRANSACCION (transaction_account_total, transaction_amount, transaction_concept, transaction_date, fk_account_id) VALUES (220.327, 200, 'Primer pago de flores', '2020-01-02', 5);
-INSERT INTO TRANSACCION (transaction_account_total, transaction_amount, transaction_concept, transaction_date, fk_account_id) VALUES (2220, -20, 'Celular nuevo', '2020-01-02', 4);
-INSERT INTO TRANSACCION (transaction_account_total, transaction_amount, transaction_concept, transaction_date, fk_account_id) VALUES (1220.32, -420.32, 'Primer cobro', '2020-01-02', 3);
+INSERT INTO TRANSACCION (transaction_account_total, transaction_amount, transaction_concept, transaction_date, transaction_create_date, fk_account_id, fk_category_id) VALUES (20.32, -10, 'Primer pago de clases', '2020-01-02', '2020-04-03', 1, 1);
+INSERT INTO TRANSACCION (transaction_account_total, transaction_amount, transaction_concept, transaction_date, transaction_create_date, fk_account_id, fk_category_id) VALUES (100.9, 120, 'Carne', '2020-04-01', '2020-04-03', 4, 5);
+INSERT INTO TRANSACCION (transaction_account_total, transaction_amount, transaction_concept, transaction_date, transaction_create_date, fk_account_id, fk_category_id) VALUES (220.327, 200, 'Primer pago de flores', '2020-01-02', '2020-04-03', 5, 4);
+INSERT INTO TRANSACCION (transaction_account_total, transaction_amount, transaction_concept, transaction_date, transaction_create_date, fk_account_id, fk_category_id) VALUES (2220, -20, 'Celular nuevo', '2020-01-02', '2020-04-03', 4, 2);
+INSERT INTO TRANSACCION (transaction_account_total, transaction_amount, transaction_concept, transaction_date, transaction_create_date, fk_account_id, fk_category_id) VALUES (1220.32, -420.32, 'Primer cobro', '2020-01-02', '2020-04-03', 3, 1);
 SELECT * FROM TRANSACCION;

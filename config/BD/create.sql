49d5f93c37d4c54bf27ea94c0d8433dc9a0f7859
Miguel Fraga <mfraga@conectium.com>
2020-03-17 20:32:48 -0400

    Modificaciones en los scripts de creacion de la BD

--- config/BD/create.sql
+++ config/BD/create.sql
@@ -2,10 +2,11 @@
 
 CREATE TABLE USUARIO (
 	user_id INTEGER NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(20) NOT NULL,
-    last_name VARCHAR(20) NOT NULL,
-    email VARCHAR(50) NOT NULL UNIQUE,
-    bd_date DATE NOT NULL,
+    user_first_name VARCHAR(20) NOT NULL,
+    user_last_name VARCHAR(20) NOT NULL,
+    user_email VARCHAR(50) NOT NULL UNIQUE,
+    user_birthdate DATE NOT NULL,
+    user_password VARCHAR(12) NOT NULL,
     PRIMARY KEY (user_id)
 );
 
@@ -25,20 +26,22 @@ CREATE TABLE MONEDA (
 
 CREATE TABLE CUENTA (
 	account_id INTEGER NOT NULL AUTO_INCREMENT,
-    account_resume DOUBLE NOT NULL,
+    account_type VARCHAR(3) NOT NULL,
     account_reference VARCHAR(6) NOT NULL,
     fk_user_id INTEGER NOT NULL,
-    fk_bank_id INTEGER NOT NULL,
+    fk_bank_id INTEGER,
     fk_currency_id INTEGER NOT NULL,
 	PRIMARY KEY (account_id),
     FOREIGN KEY FK_ACCOUNT_USER_ID (fk_user_id) REFERENCES USUARIO (user_id),
     FOREIGN KEY FK_ACCOUNT_BANK_ID (fk_bank_id) REFERENCES BANCO (bank_id),
-    FOREIGN KEY FK_ACCOUNT_CURRENCY_ID (fk_currency_id) REFERENCES MONEDA (currency_id)
+    FOREIGN KEY FK_ACCOUNT_CURRENCY_ID (fk_currency_id) REFERENCES MONEDA (currency_id),
+    CONSTRAINT CHECK_ACCOUNT_TYPE CHECK (account_type in ('DIG','EFE','CRY'))
 );
 
 CREATE TABLE TRANSACCION (
 	transaction_id INTEGER NOT NULL AUTO_INCREMENT,
-    transaction_total DOUBLE NOT NULL,
+    transaction_account_total DOUBLE NOT NULL,
+    transaction_amount DOUBLE NOT NULL,
     transaction_concept VARCHAR(150) NOT NULL,
     transaction_date DATE NOT NULL,
     fk_account_id INTEGER NOT NULL,

1b39cfe16bf68e7c795c6362e59df7dd57edadff
Miguel Fraga <mfraga@conectium.com>
2020-03-18 21:11:02 -0400

    CRUD de moneda arreglado

--- models/currency/currency-models.js
+++ models/currency/currency-models.js
@@ -7,19 +7,20 @@ module.exports = {
   },
   createCurrency: function(con, body, callback) {
     con.query(
-      "INSERT INTO MONEDA (currency_name, currency_symbol, currency_iso_code) VALUES (?, ?, ?);",
+      "INSERT INTO MONEDA (currency_name, currency_symbol, currency_iso_code, currency_amount_one_dollar_equivalent) VALUES (?, ?, ?, ?);",
       [
         body.currency_name,
         body.currency_symbol,
-        body.currency_iso_code
+        body.currency_iso_code,
+        body.currency_amount_one_dollar_equivalent
       ],
       callback
     );
   },
   updateCurrency: function(con, id, body, callback) {
     con.query(
-      "UPDATE MONEDA SET currency_name = ?, currency_symbol = ?, currency_iso_code = ? WHERE currency_id = ?;",
-      [body.currency_name, body.currency_symbol, body.currency_iso_code, id],
+      "UPDATE MONEDA SET currency_name = ?, currency_symbol = ?, currency_iso_code = ?, currency_amount_one_dollar_equivalent = ? WHERE currency_id = ?;",
+      [body.currency_name, body.currency_symbol, body.currency_iso_code, body.currency_amount_one_dollar_equivalent, id],
       callback
     );
   },

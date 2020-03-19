5826a122d9a3c1a3ea8e13edaef7c437982e5cd4
Miguel Fraga <mfraga@conectium.com>
2020-03-19 00:11:56 -0400

    CRUD Transaccion arreglado

--- models/transaction/transaction-models.js
+++ models/transaction/transaction-models.js
@@ -7,26 +7,30 @@ module.exports = {
   },
   createTransaction: function(con, body, callback) {
     con.query(
-      "INSERT INTO TRANSACCION (transaction_account_total, transaction_amount, transaction_concept, transaction_date, fk_account_id) VALUES (?, ?, ?, ?, ?);",
+      "INSERT INTO TRANSACCION (transaction_account_total, transaction_amount, transaction_concept, transaction_date, transaction_create_date, fk_account_id, fk_category_id) VALUES (?, ?, ?, ?, ?, ?, ?);",
       [
         body.transaction_account_total,
         body.transaction_amount,
         body.transaction_concept,
         body.transaction_date,
-        body.fk_account_id
+        body.transaction_create_date,
+        body.fk_account_id,
+        body.fk_category_id
       ],
       callback
     );
   },
   updateTransaction: function(con, id, body, callback) {
     con.query(
-      "UPDATE TRANSACCION SET transaction_account_total = ?, transaction_amount = ?, transaction_concept = ?, transaction_date = ?, fk_account_id = ? WHERE transaction_id = ?;",
+      "UPDATE TRANSACCION SET transaction_account_total = ?, transaction_amount = ?, transaction_concept = ?, transaction_date = ?, transaction_create_date = ?, fk_account_id = ?, fk_category_id = ? WHERE transaction_id = ?;",
       [
         body.transaction_account_total,
         body.transaction_amount,
         body.transaction_concept,
         body.transaction_date,
+        body.transaction_create_date,
         body.fk_account_id,
+        body.fk_category_id,
         id
       ],
       callback

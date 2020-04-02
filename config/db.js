323aa80e806b8cd8b20e8ba454b6544e778271fa
Miguel Fraga <mapena.16@est.ucab.edu.ve>
2020-04-01 23:09:45 -0400

    Cambios para deploy en HK

--- config/db.js
+++ config/db.js
@@ -1,11 +1,18 @@
 require("dotenv").config();
-const fs = require("fs");
 const pgp = require("pg-promise")();
 
 module.exports = pgp({
-  connectionString: `postgres://${process.env.DB_USER_HK}:${process.env.DB_USER_HKPW}@${process.env.DB_HOST_HK}:${process.env.DB_PORT}/${process.env.DB_HKNAME}`,
+  connectionString: DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
     mode: require
   }
 });
+
+// module.exports = pgp({
+//   connectionString: `postgres://${process.env.DB_USER_HK}:${process.env.DB_USER_HKPW}@${process.env.DB_HOST_HK}:${process.env.DB_PORT}/${process.env.DB_HKNAME}`,
+//   ssl: {
+//     rejectUnauthorized: false,
+//     mode: require
+//   }
+// });
--- server.js
+++ server.js
@@ -4,3 +4,20 @@ const port = process.env.PORT;
 app.listen(port, () =>
   console.log(`Servicio activo - (Puerto para peticiones: ${port})`)
 );
+
+// const app = require("./app.js");
+// const https = require("https");
+// const port = process.env.PORT;
+// const fs = require("fs");
+
+// https
+//   .createServer(
+//     {
+//       key: fs.readFileSync("server.key"),
+//       cert: fs.readFileSync("server.cert")
+//     },
+//     app
+//   )
+//   .listen(port, function() {
+//     console.log(`Listening on port ${port}! Go to https://localhost:${port}/`);
+//   });

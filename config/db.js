ea58dcfeee07932ef2d07324f054a300c49a5aaa
Miguel Fraga <mapena.16@est.ucab.edu.ve>
2020-04-01 22:35:40 -0400

    Cambios realizados en .env para seguridad y deploy en Heroku

--- config/db.js
+++ config/db.js
@@ -3,8 +3,7 @@ const fs = require("fs");
 const pgp = require("pg-promise")();
 
 module.exports = pgp({
-  connectionString:
-    "postgres://vhndxbrzealghr:0a4d1b0e814a0aebc5dec914ddeaf1f6d22ffe2ddbb1fa39d56bb3f51bf43aec@ec2-54-147-209-121.compute-1.amazonaws.com:5432/dckk0n1b8bv118",
+  connectionString: `postgres://${process.env.DB_USER_HK}:${process.env.DB_USER_HKPW}@${process.env.DB_HOST_HK}:${process.env.DB_PORT}/${process.env.DB_HKNAME}`,
   ssl: {
     rejectUnauthorized: false,
     mode: require

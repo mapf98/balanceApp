b5a94393616e3748f0440aa7564abd15623791c9
Miguel Fraga <mapena.16@est.ucab.edu.ve>
2020-04-01 22:24:47 -0400

    Cambios en el string de conexion, ya bd en heroku disponible

--- config/db.js
+++ config/db.js
@@ -1,6 +1,12 @@
 require("dotenv").config();
+const fs = require("fs");
 const pgp = require("pg-promise")();
 
-module.exports = pgp(
-  `postgres://${process.env.DB_USER_PG}:${process.env.DB_PASSWORD}@localhost:5432/balanceapp`
-);
+module.exports = pgp({
+  connectionString:
+    "postgres://vhndxbrzealghr:0a4d1b0e814a0aebc5dec914ddeaf1f6d22ffe2ddbb1fa39d56bb3f51bf43aec@ec2-54-147-209-121.compute-1.amazonaws.com:5432/dckk0n1b8bv118",
+  ssl: {
+    rejectUnauthorized: false,
+    mode: require
+  }
+});

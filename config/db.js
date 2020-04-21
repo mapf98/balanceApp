251cef0e3414ea7f70551531ecf6204b83612b9c
Miguel Fraga <mapena.16@est.ucab.edu.ve>
2020-04-21 18:10:34 -0400

    Subida a prodcuccion

--- config/db.js
+++ config/db.js
@@ -2,15 +2,15 @@ require("dotenv").config();
 const pgp = require("pg-promise")();
 
 //Development
-module.exports = pgp({
-  connectionString: `postgres://${process.env.DB_USER_PG}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
-});
+// module.exports = pgp({
+//   connectionString: `postgres://${process.env.DB_USER_PG}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+// });
 
 //Production
-// module.exports = pgp({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//     mode: require
-//   }
-// });
+module.exports = pgp({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+    mode: require
+  }
+});
